03e50183daaaffbcc5109587e00c1cbcd82f639d
susu10-10 <successchukwu20@gmail.com>
2026-07-29 01:45:24 +0200

    fix(terraform): remove unused GitHub token variable and related resources

--- terraform/cluster/variable.tf
+++ terraform/cluster/variable.tf
@@ -1,8 +1,4 @@
-variable "gitops_token" {
-  description = "GitHub personal access token with repo write access"
-  type        = string
-  sensitive   = true
-}
+
 
 variable "do_token" {
   description = "Digital Ocean personal access token"

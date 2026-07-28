729afc737de014c0be70caf0d35da9dbe5d90425
susu10-10 <successchukwu20@gmail.com>
2026-07-28 13:10:00 +0200

    fix(terraform): update Kubernetes namespace resource type to v1

--- terraform/cluster/main.tf
+++ terraform/cluster/main.tf
@@ -1,18 +1,18 @@
 # namespaces for platform components
 
-resource "kubernetes_namespace" "argocd" {
+resource "kubernetes_namespace_v1" "argocd" {
   metadata {
     name = "argocd"
   }
 }
 
-resource "kubernetes_namespace" "platform" {
+resource "kubernetes_namespace_v1" "platform" {
   metadata {
     name = "platform"
   }
 }
 
-resource "kubernetes_namespace" "boutique" {
+resource "kubernetes_namespace_v1" "boutique" {
   metadata {
     name = "boutique"
     labels = {
@@ -26,11 +26,11 @@ resource "helm_release" "argocd" {
   name       = "argocd"
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
-  namespace  = kubernetes_namespace.argocd.metadata[0].name
+  namespace  = kubernetes_namespace_v1.argocd.metadata[0].name
   version    = "10.2.1"
 
   values = [
     file("${path.module}/../../clusters/boutique/argocd/values.yaml")
   ]
-  depends_on = [kubernetes_namespace.argocd]
+  depends_on = [kubernetes_namespace_v1.argocd]
 }
